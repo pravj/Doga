1a2f4515823438516670b3f3bd5790e9fe7d88d6
Pravendra Singh <hackpravj@gmail.com>
2014-09-08 16:50:16 +0530

    Statistics: notify about alert status in realtime
    > update alert_template in realtime as soon as system is under high
      traffic or in recovered state.

--- Doga/statistics.py
+++ Doga/statistics.py
@@ -4,17 +4,19 @@
 Doga.statistics
 
 This module manage information about log statistics
-Shows data summary periodically in each 10 seconds
+update and keep track of usage summary periodically
 """
 
+import time
 import threading
 from collections import Counter
 
 from configer import value
 from thread_timer import ThreadTimer
+#from gui import DogaGUI
 
 
-class Statistics():
+class Statistics:
 
     def __init__(self):
         self.queue = []
@@ -27,11 +29,22 @@ class Statistics():
         self.stats_timer = ThreadTimer(10, self.stop_event, self.update_queue)
         self.stats_timer.start()
 
-        self.alert_timer = ThreadTimer(120, self.stop_event, self.update_alert_queue)
+        self.alert_timer = ThreadTimer(30, self.stop_event, self.update_alert_queue)
         self.alert_timer.start()
 
         self.stats_scanner = ThreadTimer(1, self.stop_event, self.check_stats)
         self.stats_scanner.start()
+
+        self.stats_template = ""
+        self.alert_template = "Traffic status: Normal, Alert state: No"
+
+        self.is_alert = False
+
+        self.alert_start = ""
+        self.alert_end = ""
+
+        #self.app = DogaGUI(self)
+        #self.app.run()
 
     def queue_event(self, method, host, section):
         """ Queue each request to be used in statistics
@@ -44,28 +57,26 @@ class Statistics():
         self.queue.append(host + section)
         self.alert_queue.append(host + section)
 
-    def max_queue(self):
+    def update_statistics(self):
         """ return resource section info having maximum hits and count of total hits
         """
 
+        self.total += len(self.queue)
+
+        status = "[No recent requests]"
         if (len(self.queue) > 0):
             counts = Counter(self.queue)
             frequency = counts.most_common()
 
-            print frequency[0][0], frequency[0][1]
-        else:
-            print "No requests"
+            status = "[%s : %d]" % (frequency[0][0], frequency[0][1])
 
-        self.total += len(self.queue)
-
-        print len(self.queue)
-        print self.total
+        self.stats_template = "Maximum : %s, Recent : %d, Total : %d" % (status, len(self.queue), self.total)
 
     def update_queue(self):
         """ update the queue periodically and call for 'max_queue' method
         """
 
-        self.max_queue()
+        self.update_statistics()
         self.queue = []
 
     def update_alert_queue(self):
@@ -78,9 +89,17 @@ class Statistics():
         """ call for an alert if request count is not in threshold range
         """
 
-        minimum, maximum = value('minimum'), value('maximum')
+        maximum = int(value('maximum'))
 
-        if (len(self.alert_queue) > maximum):
-            print "Alert : requests more than maximum %d" % (len(self.alert_queue))
-        if (len(self.alert_queue) < minimum):
-            print "Alert : request less than minimum %d" % (len(self.alert_queue))
+        # we are in alert state
+        if (self.is_alert):
+            if (len(self.alert_queue) < maximum):
+                self.is_alert = False
+                self.alert_end = time.strftime("%H:%M:%S")
+                self.alert_status = "Traffic status: Normal Alert state: No Max Hits: %d Recovered at: %s" % (len(self.alert_queue), self.alert_end)
+        # we are alert free now
+        else:
+            if (len(self.alert_queue) > maximum):
+                self.is_alert = True
+                self.alert_start = time.strftime("%H:%M:%S")
+                self.alert_status = "Traffic status: High Alert state: Yes Max Hits: %d Triggered at: %s" % (len(self.alert_queue), self.alert_start)
